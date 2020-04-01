09f1da1fece25778dcced427b621b97b3662ac9e
Mohammed-Umar <md.umar05@gmail.com>
2020-04-01 08:01:25 +0530

    Set Tutorial order for temp use

--- src/Tutorials/index.ts
+++ src/Tutorials/index.ts
@@ -2,12 +2,18 @@ import Tutorial1Contents from './Tutorial1'
 import Tutorial2Contents from './Tutorial2'
 import Tutorial3Contents  from './Tutorial3'
 import Tutorial4Contents from './Tutorial4'
+import Tutorial5Contents from './Tutorial5'
+import Tutorial6Contents from './Tutorial6'
+import Tutorial7Contents from './Tutorial7'
 
 export const tutorialConstructors = [
+  Tutorial7Contents,
+  Tutorial6Contents,
+  Tutorial5Contents,
   Tutorial4Contents,
   Tutorial3Contents,
-  Tutorial1Contents,
   Tutorial2Contents,
+  Tutorial1Contents,
 ]
 
 export { TutorialContents, TutorialState } from './Common'
